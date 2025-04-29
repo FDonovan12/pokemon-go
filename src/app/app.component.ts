85ca089a0dbefb9ece313395d3c43a7fdab23512
donovan <donovanferreira12@gmail.com>
2025-04-29 17:09:53 +0200

    Deploy ?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pokemon-go';
+  copy(string: string) {
+    navigator.clipboard
+      .writeText(string)
+      .then(() => {
+        console.log('Text copied to clipboard:', string);
+      })
+      .catch((err) => {
+        console.error('Error copying text: ', err);
+      });
+  }
 }
