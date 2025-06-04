ccffe3d1319b0fdfcd32cf7aae24b127fa86939d
FDonovan12 <donovanferreira12@gmail.com>
2025-06-04 05:43:34 +0200

    add locale fr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideHttpClient(), provideRouter(routes, withComponentInputBinding())],
+    providers: [
+        provideHttpClient(),
+        provideRouter(routes, withComponentInputBinding()),
+        { provide: LOCALE_ID, useValue: 'fr-FR' },
+    ],
 };
