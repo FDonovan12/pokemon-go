918cf510af30301aee970c9e8379ce8c0bbd4f37
FDonovan12 <donovanferreira12@gmail.com>
2025-07-28 12:28:16 +0200

    add copy to event pages 2

--- src/app/pages/event/event.component.ts
+++ src/app/pages/event/event.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, input, InputSignal } from '@angular/core';
 import { Router } from '@angular/router';
 import { EventPokemon } from '@entities/event';
 import { EventRepository } from '@repositories/event/event.repository';
+import { ClipboardService } from '@services/clipboard-service';
+import { FilterService } from '@services/filter-service/filter-service';
 import { Logger } from '@services/logger/logger';
 import { SavageGroupComponent } from './savage-group/savage-group';
 
@@ -20,6 +22,10 @@ export class EventComponent {
     private readonly router: Router = inject(Router);
 
     private readonly logger = inject(Logger);
+
+    readonly clipboardService = inject(ClipboardService);
+
+    readonly filterService = inject(FilterService);
 
     event!: EventPokemon;
 
