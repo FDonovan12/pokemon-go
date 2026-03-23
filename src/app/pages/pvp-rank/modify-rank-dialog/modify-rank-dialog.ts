3931d900c0e2f9aef1960b6848e10206cf0f0b40
FDonovan12 <donovanferreira12@gmail.com>
2026-03-23 14:49:31 +0100

    modify user experience on modify rank pvp

--- src/app/pages/pvp-rank/modify-rank-dialog/modify-rank-dialog.ts
+++ src/app/pages/pvp-rank/modify-rank-dialog/modify-rank-dialog.ts
@@ -1,11 +1,13 @@
 // modify-rank-dialog.component.ts
-import { Component, EventEmitter, model, Output, signal } from '@angular/core';
+import { afterNextRender, Component, ElementRef, EventEmitter, model, Output, signal, viewChild } from '@angular/core';
 import { Form, League } from '../pvp-rank';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-modify-rank-dialog',
     templateUrl: './modify-rank-dialog.html',
     styleUrl: './modify-rank-dialog.css',
+    imports: [FormsModule],
 })
 export class ModifyRankDialogComponent {
     rank = signal<number>(1);
@@ -14,6 +16,18 @@ export class ModifyRankDialogComponent {
 
     @Output() submitted = new EventEmitter<{ rank: number; league: League; form: Form }>();
     @Output() cancelled = new EventEmitter<void>();
+
+    rankInput = viewChild<ElementRef<HTMLInputElement>>('rankInput');
+
+    constructor() {
+        afterNextRender(() => {
+            const input = this.rankInput()?.nativeElement;
+            if (input) {
+                input.focus();
+                input.select();
+            }
+        });
+    }
 
     confirm() {
         if (this.rank() < 1 || this.rank() > 4096) return alert('Rank doit être entre 1 et 4096');
