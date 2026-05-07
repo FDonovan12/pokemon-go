5fe26a7df2ae6b835bed9fc27b85cb418efc4ef0
FDonovan12 <donovanferreira12@gmail.com>
2026-05-07 02:22:50 +0200

    update @angular/core

--- src/app/shared/features/toast/components/toast-container.component.ts
+++ src/app/shared/features/toast/components/toast-container.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 import { ToastService } from '../toast.service';
 import { ToastComponent } from './toast.component';
 
 @Component({
     selector: 'app-toast-container',
-    imports: [CommonModule, ToastComponent],
+    imports: [ToastComponent],
     templateUrl: './toast-container.component.html',
     styleUrl: './toast-container.component.css',
 })
