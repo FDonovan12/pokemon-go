17b756bbc4755820a4486bcdf6e9e31ebd6646b3
FDonovan12 <donovanferreira12@gmail.com>
2026-05-27 09:00:06 +0200

    fix issue with dynamax search

--- src/app/pages/dynamax/dynamax.page.ts
+++ src/app/pages/dynamax/dynamax.page.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ImagePokemon } from 'app/shared/components/image-pokemon/image-pokemon';
 import { PercentColor } from 'app/shared/components/percent-color/percent-color';
 import { TypeComponent } from 'app/shared/components/type/type.component';
@@ -9,7 +10,7 @@ import { DynamaxStore } from './dynamax-store/dynamax-store';
 @MeasurePerf()
 @Component({
     selector: 'app-dynamax.page',
-    imports: [TypeComponent, ImagePokemon, PercentColor],
+    imports: [TypeComponent, ImagePokemon, PercentColor, FormsModule, ReactiveFormsModule],
     templateUrl: './dynamax.page.html',
     styleUrl: './dynamax.page.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,4 +23,12 @@ export class DynamaxPage {
     test = this.dynamaxStore.finalAllDynamaxPokemonResultDamageBase();
 
     maxDamage = this.dynamaxStore.maxDamageFind();
+
+    search = this.dynamaxStore.search;
+
+    // constructor() {
+    //     effect(() => {
+    //         this.dynamaxStore.setSearch(this.search() ?? '');
+    //     });
+    // }
 }
