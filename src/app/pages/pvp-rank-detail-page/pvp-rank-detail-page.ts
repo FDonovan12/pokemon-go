03e2de5de093cf1a8b6efbf0c359a855dfd6ff2f
FDonovan12 <donovanferreira12@gmail.com>
2026-07-06 09:38:40 +0200

    modify some css and refactor sources

--- src/app/pages/pvp-rank-detail-page/pvp-rank-detail-page.ts
+++ src/app/pages/pvp-rank-detail-page/pvp-rank-detail-page.ts
@@ -7,14 +7,6 @@ import { ImagePokemon } from '@shared/components/image-pokemon/image-pokemon';
 import { PvpRank, PVPRankStore } from '../pvp-rank/pvp-rank-store/pvp-rank-store';
 import { League } from '../pvp-rank/pvp-rank.type';
 
-const MIN_IVS = {
-    meilleur: 0,
-    meteo: 4,
-    echange: 5,
-    raid: 10,
-    chanceux: 12,
-} as const;
-type Source = keyof typeof MIN_IVS;
 type RankedStat = {
     rank: number;
     stat: LeagueStats;
@@ -22,9 +14,25 @@ type RankedStat = {
     totalCombos: number;
     percentage: number;
 };
-const SOURCE_COMBOS = Object.fromEntries(
-    Object.entries(MIN_IVS).map(([source, minIv]) => [source, (16 - minIv) ** 3]),
-) as Record<Source, number>;
+
+const createSource = (minIv: number, label: string, icon: string) => ({
+    minIv,
+    combos: (16 - minIv) ** 3,
+    label,
+    icon,
+});
+
+const SOURCES = {
+    meilleur: createSource(0, 'Rank 1', '⭐'),
+    meteo: createSource(4, 'Météo', '🌦️'),
+    echange: createSource(5, 'Échange', '🔄'),
+    raid: createSource(10, 'Raid / œuf', '⚔️'),
+    chanceux: createSource(12, 'Chanceux', '🍀'),
+} as const;
+
+type Source = keyof typeof SOURCES;
+// type Source = keyof typeof MIN_IVS;
+
 @Component({
     selector: 'app-pvp-rank-detail-page',
     imports: [ImagePokemon, NgTemplateOutlet],
@@ -32,6 +40,11 @@ const SOURCE_COMBOS = Object.fromEntries(
     styleUrl: './pvp-rank-detail-page.css',
 })
 export class PvpRankDetailPage {
+    readonly sources = Object.entries(SOURCES).map(([key, value]) => ({
+        key: key as Source,
+        ...value,
+    }));
+
     slug: Signal<PokemonSlug> = input.required<PokemonSlug>();
     private readonly _pokemonRepository: PokemonRepository = inject(PokemonRepository);
     private readonly _pVPRankStore = inject(PVPRankStore);
@@ -47,14 +60,7 @@ export class PvpRankDetailPage {
     });
 
     actualRank: Signal<PvpRank | undefined> = computed(() => this._pVPRankStore.allRank().get(this.slug()));
-    getRankDelta(source: Source, league: 'great' | 'ultra'): 'better' | 'worse' | 'same' | null {
-        const actual = league === 'great' ? this.actualRank()?.super?.normal : this.actualRank()?.hyper?.normal;
-        const best = this.bestRanks()?.[league]?.[source];
-        if (!actual || !best) return null;
-        if (best.rank < actual) return 'better';
-        if (best.rank > actual) return 'same';
-        return 'same';
-    }
+
     bestRanks = computed(() => {
         const data = this.rankPVP.value();
         const table = this._pokemonRepository.cpMultiplier.value();
@@ -66,21 +72,27 @@ export class PvpRankDetailPage {
         const getBestRank = (league: League, available: boolean): Record<Source, RankedStat | null> | null => {
             const ranks = data[league];
             if (!available) return null;
-            return Object.fromEntries(
-                Object.entries(MIN_IVS).map(([source, minIv]) => {
-                    const index = ranks.findIndex((r) => r.atk >= minIv && r.def >= minIv && r.sta >= minIv);
-                    if (index === -1) return [source, null];
+            const resultList = this.sources.map((source) => {
+                const minIv = source.minIv;
+                const index = ranks.findIndex((r) => r.atk >= minIv && r.def >= minIv && r.sta >= minIv);
+                if (index === -1) return { key: source.key, value: null };
 
-                    const sliced = ranks.slice(0, this.actualRank()?.[league].normal ?? 4096);
-                    const betterCount = sliced.filter((r) => r.atk >= minIv && r.def >= minIv && r.sta >= minIv).length;
-                    console.log(sliced.length, betterCount, index);
+                const sliced = ranks.slice(0, this.actualRank()?.[league].normal ?? 4096);
+                const betterCount = sliced.filter((r) => r.atk >= minIv && r.def >= minIv && r.sta >= minIv).length;
 
-                    const totalCombos = SOURCE_COMBOS[source as Source];
-                    const percentage = Math.round((betterCount / totalCombos) * 1000) / 10;
+                const totalCombos = source.combos;
+                const percentage = Math.round((betterCount / totalCombos) * 1000) / 10;
 
-                    return [source, { rank: index + 1, stat: ranks[index], betterCount, totalCombos, percentage }];
-                }),
-            ) as Record<Source, RankedStat | null>;
+                return {
+                    key: source.key,
+                    value: { rank: index + 1, stat: ranks[index], betterCount, totalCombos, percentage },
+                };
+            });
+            const resultObject = resultList.toObject(
+                (obj) => obj.key,
+                (obj) => obj.value,
+            );
+            return resultObject;
         };
 
         return {
@@ -97,28 +109,20 @@ export class PvpRankDetailPage {
             }
         });
     }
-    readonly sources = Object.keys(MIN_IVS) as Source[];
-
-    readonly sourceLabels: Record<Source, string> = {
-        meilleur: 'Rank 1',
-        meteo: 'Météo',
-        echange: 'Échange',
-        raid: 'Raid / œuf',
-        chanceux: 'Chanceux',
-    };
-
-    readonly sourceIcons: Record<Source, string> = {
-        meilleur: '⭐',
-        meteo: '🌦️',
-        echange: '🔄',
-        raid: '⚔️',
-        chanceux: '🍀',
-    };
 
     @HostListener('click', ['$event'])
     onHostClick(event: MouseEvent): void {
         if (event.target === event.currentTarget) {
             this._router.navigate(['pvp-rank']);
         }
+    }
+
+    getRankDelta(source: Source, league: 'great' | 'ultra'): 'better' | 'worse' | 'same' | null {
+        const actual = league === 'great' ? this.actualRank()?.super?.normal : this.actualRank()?.hyper?.normal;
+        const best = this.bestRanks()?.[league]?.[source];
+        if (!actual || !best) return null;
+        if (best.rank < actual) return 'better';
+        if (best.rank > actual) return 'worse';
+        return 'same';
     }
 }
