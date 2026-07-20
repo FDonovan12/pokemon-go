1ee26579f70f1c770c4416ef2e0a8bd72f1c26aa
FDonovan12 <donovanferreira12@gmail.com>
2026-07-20 20:56:52 +0200

    fix blanck part a the end of dynamax page

--- src/app/pages/dynamax/dynamax.page.ts
+++ src/app/pages/dynamax/dynamax.page.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { allTypes } from './../../entities/pokemon';
-import { DynamaxStore } from './dynamax-store/dynamax-store';
 import { ImagePokemon } from '../../shared/components/image-pokemon/image-pokemon';
 import { PercentColor } from '../../shared/components/percent-color/percent-color';
 import { TypeComponent } from '../../shared/components/type/type.component';
 import { MeasurePerf } from '../../shared/decorator/measure-perf';
+import { allTypes } from './../../entities/pokemon';
+import { DynamaxStore } from './dynamax-store/dynamax-store';
 
 @MeasurePerf()
 @Component({
